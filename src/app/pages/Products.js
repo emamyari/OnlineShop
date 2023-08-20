9f78997b9d411bed9bc22153051ccf91c4d4926f
Elay <li0elay0li@gmail.com>
2023-08-20 08:12:31 -0700

    products

--- src/app/pages/Products.js
+++ src/app/pages/Products.js
@@ -1,9 +1,47 @@
 import React from "react";
 import {useSubheader} from "../../_metronic/layout";
 
+import {
+  Button,
+  Card,
+  ListGroup,
+  ListGroupItem,
+  Nav,
+  CardGroup,
+  CardDeck,
+  CardColumns
+} from "react-bootstrap";
 export const Products = () => {
   const suhbeader = useSubheader();
   suhbeader.setTitle("Products");
 
-  return (<>Products</>);
+  return (<div className="row">
+  
+<Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="https://sanvarifashion.com/images/product/2021/11/tummy-cover-ikat-low-rate-blous-collection-2021-11-13_13_39_29.jpeg" />
+  <Card.Body>
+    <Card.Title>Tank Top</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Button variant="success">add to cart</Button>
+  </Card.Body>
+</Card>
+<>   </>
+<Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="https://sanvarifashion.com/images/product/2021/11/tummy-cover-ikat-low-rate-blous-collection-2021-11-13_13_39_29.jpeg" />
+  <Card.Body>
+    <Card.Title>Tank Top</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Button variant="success">add to cart</Button>
+  </Card.Body>
+</Card>
+
+
+
+  </div>);
 };
