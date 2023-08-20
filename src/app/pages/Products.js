166837339688503848dce31a200d16b08a804320
Elay <li0elay0li@gmail.com>
2023-08-20 08:55:49 -0700

    farsi

--- src/app/pages/Products.js
+++ src/app/pages/Products.js
@@ -13,31 +13,29 @@ import {
 } from "react-bootstrap";
 export const Products = () => {
   const suhbeader = useSubheader();
-  suhbeader.setTitle("Products");
+  suhbeader.setTitle("صفحه محصولات");
 
   return (<div className="row">
   
 <Card className="m-5" style={{ width: '18rem'}}>
   <Card.Img variant="top" src="https://sanvarifashion.com/images/product/2021/11/tummy-cover-ikat-low-rate-blous-collection-2021-11-13_13_39_29.jpeg" />
   <Card.Body>
-    <Card.Title>Tank Top</Card.Title>
+    <Card.Title>تاپ تابستانه</Card.Title>
     <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
+    پر کردن پر کردن پر کردن پر کردن پر کردن پر کردن پر کردن پر کردن پر کردن پر کردن 
     </Card.Text>
-    <Button variant="success">add to cart</Button>
+    <Button variant="success">افزودن به سبد</Button>
   </Card.Body>
 </Card>
 <>   </>
 <Card className="m-5" style={{ width: '18rem' }}>
   <Card.Img variant="top" src="https://sanvarifashion.com/images/product/2021/11/tummy-cover-ikat-low-rate-blous-collection-2021-11-13_13_39_29.jpeg" />
   <Card.Body>
-    <Card.Title>Tank Top</Card.Title>
+    <Card.Title>تاپ تابستانه</Card.Title>
     <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
+    پر کردن پر کردن پر کردن پر کردن پر کردن پر کردن پر کردن پر کردن پر کردن پر کردن 
     </Card.Text>
-    <Button variant="success">add to cart</Button>
+    <Button variant="success">افزودن به سبد</Button>
   </Card.Body>
 </Card>
 
