1bbc9598e6d14105b41f284926058fe57e22512b
cactus1386 <cactusinjast@gmail.com>
2023-10-22 20:42:54 -0700

    add filter and slider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
 import ProductList from './components/ProductList';
+import ProductInfo from './components/Products';
+import Login from './components/Login';
+import Header from './components/header';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Filter from './components/filter';
+import Slider from './components/Slider';
 
 function App() {
   return (
-    <div className="App">
-      <ProductList />
-    </div>
+    <Router>
+        <Routes>
+          <Route path='/' element={<ProductList />} />
+          <Route path='/detail' element={<ProductInfo />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/header' element={<Header />} />
+          <Route path='/filter' element={<Filter />} />
+          <Route path='/slider' element={<Slider />} />
+        </Routes>
+    </Router>
   );
 }
 
