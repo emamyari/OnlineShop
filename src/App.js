ac85516cf8b293a49625ec411facd98592e16b18
Elay <li0elay0li@gmail.com>
2024-05-12 20:05:49 -0700

    Menu

--- src/App.js
+++ src/App.js
@@ -10,25 +10,33 @@ import Slider from './components/Slider';
 import Info from './components/PersonalInfo';
 import RPP from './components/RecentlyPurchasedPrdcts';
 import MiniSlider from './components/MinilSlider';
-
+import Nav from './components/Nav';
+import MenuButton from './components/NanMenue';
+import ShoppingCartPage from './components/KharidSabad';
 
 function App() {
+  const menuItems = ['Item 1', 'Item 2', 'Item 3'];
   return (
-    <Router>
-        <Routes>
-          <Route path='/' element={<ProductList />} />
-          <Route path='/detail' element={<ProductInfo />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/header' element={<Header />} />
-          <Route path='/filter' element={<Filter />} />
-          <Route path='/slider' element={<Slider />} />
-          <Route path='/cart' element={<Cart />} />
-          <Route path='/info' element={<Info />} />
-          <Route path='/rpp' element={<RPP/>} />
-        </Routes>
     
-    </Router>
-    
+    <><Router>
+      <Nav />
+      <Routes>
+        <Route path='/' element={<ProductList />} />
+        <Route path='/detail' element={<ProductInfo />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/header' element={<Header />} />
+        <Route path='/filter' element={<Filter />} />
+        <Route path='/slider' element={<Slider />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/info' element={<Info />} />
+        <Route path='/rpp' element={<RPP />} />
+        <Route path='/sabad' element={<ShoppingCartPage />} />
+      </Routes>
+
+    </Router><div>
+        <h1>My App</h1>
+        <MenuButton items={menuItems}/>
+      </div></>
   );
 }
 
